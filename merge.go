fb48d8828aaa3523820154eab61e9313b4b4db70
chrisghill <chris@massdriver.cloud>
2023-03-02 00:18:50 -0700

    README

--- merge.go
+++ merge.go
@@ -24,37 +24,3 @@ func MergeSchemas(base, merge *Schema) (*Schema, error) {
 
 	return merged, nil
 }
-
-// func MergeSchemas(base, merge *Schema) (*Schema, error) {
-// 	merged := map[string]interface{}{}
-// 	result := new(Schema)
-
-// 	jsonBase, err := json.Marshal(base)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	err = json.Unmarshal(jsonBase, &merged)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	jsonMerge, err := json.Marshal(merge)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	err = json.Unmarshal(jsonMerge, &merged)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	jsonResult, err := json.Marshal(merged)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	err = json.Unmarshal(jsonResult, result)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	return result, nil
-// }
